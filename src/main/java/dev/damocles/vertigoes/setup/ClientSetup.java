200e471bd4ccfd2d3a09cdb2d5b9538d9a8365d0
Damocles <38077049+damoc1es@users.noreply.github.com>
2022-08-25 18:53:56 +0300

    initial commit (working, but lacking features)

---
+++ src/main/java/dev/damocles/vertigoes/setup/ClientSetup.java
@@ -0,0 +1,14 @@
+package dev.damocles.vertigoes.setup;
+
+import dev.damocles.vertigoes.Vertigoes;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = Vertigoes.MODID, value= Dist.CLIENT, bus=Mod.EventBusSubscriber.Bus.MOD)
+public class ClientSetup {
+
+    public static void init(FMLClientSetupEvent event) {
+
+    }
+}
---
+++ src/main/java/dev/damocles/vertigoes/setup/Registration.java
@@ -0,0 +1,37 @@
+package dev.damocles.vertigoes.setup;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FlowerBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.effect.*;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static dev.damocles.vertigoes.Vertigoes.MODID;
+
+public class Registration {
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public static void init() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        BLOCKS.register(bus);
+        ITEMS.register(bus);
+    }
+
+    public static final Item.Properties ITEM_PROPERTIES = new Item.Properties().tab(ModSetup.ITEM_GROUP);
+
+    //public static final RegistryObject<Block> MYOSOTIS = BLOCKS.register("myosotis", () -> new FlowerBlock(MobEffects.REGENERATION, 100, BlockBehaviour.Properties.of(Material.STONE)));
+    public static final RegistryObject<Block> MYOSOTIS = BLOCKS.register("myosotis", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
+    public static final RegistryObject<Item> MYOSOTIS_ITEM = fromBlock(MYOSOTIS);
+
+    public static <B extends Block> RegistryObject<Item> fromBlock(RegistryObject<B> block) {
+        return ITEMS.register(block.getId().getPath(), () -> new BlockItem(block.get(), ITEM_PROPERTIES));
+    }
+}
