8288f9ac3e2fafdee2299784fd3ea45c837f1428
Damocles <38077049+damoc1es@users.noreply.github.com>
2022-08-30 16:14:36 +0300

    now myosotis stores the coords and dimension of last placement

--- src/main/java/dev/damocles/vertigoes/block/MyosotisBlock.java
+++ src/main/java/dev/damocles/vertigoes/block/MyosotisBlock.java
@@ -1,11 +1,69 @@
 package dev.damocles.vertigoes.block;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.FlowerBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class MyosotisBlock extends FlowerBlock {
-    float coordX=0, coordY=0, coordZ=0;
-    public MyosotisBlock(MobEffect p_53512_, int p_53513_, Properties p_53514_) {
-        super(p_53512_, p_53513_, p_53514_);
+    public MyosotisBlock(MobEffect suspiciousStewEffect, int effectDuration, Properties properties) {
+        super(suspiciousStewEffect, effectDuration, properties);
+    }
+
+    @Override
+    public void playerDestroy(@NotNull Level level, @NotNull Player player, @NotNull BlockPos pos, @NotNull BlockState blockState, @Nullable BlockEntity blockEntity, @NotNull ItemStack tool) {
+        player.awardStat(Stats.BLOCK_MINED.get(this));
+
+        // Block.dropResources
+        if (level instanceof ServerLevel) {
+            getDrops(blockState, (ServerLevel)level, pos, blockEntity, player, tool).forEach((itemDrop) -> {
+                // Set tag of drop with the coordinates and dimension of last placement
+                CompoundTag tags = new CompoundTag();
+                tags.putBoolean("vertigoes.wasPlaced", true);
+                tags.putInt("vertigoes.coordX", pos.getX());
+                tags.putInt("vertigoes.coordY", pos.getY());
+                tags.putInt("vertigoes.coordZ", pos.getZ());
+                tags.putString("vertigoes.dim", level.dimension().location().toString());
+
+                itemDrop.setTag(tags);
+                popResource(level, pos, itemDrop);
+            });
+            blockState.spawnAfterBreak((ServerLevel)level, pos, tool);
+        }
+    }
+
+    @Override
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable BlockGetter level, @NotNull List<Component> tooltip, @NotNull TooltipFlag flag) {
+        // Setting the tooltip depending on if it was ever placed
+        if(stack.hasTag() && stack.getTag() != null) {
+            CompoundTag currentTags = stack.getTag();
+            if(currentTags.contains("vertigoes.wasPlaced") && currentTags.getBoolean("vertigoes.wasPlaced")) { //
+                int coordX = currentTags.getInt("vertigoes.coordX");
+                int coordY = currentTags.getInt("vertigoes.coordY");
+                int coordZ = currentTags.getInt("vertigoes.coordZ");
+                String dim = currentTags.getString("vertigoes.dim");
+                if(dim.contains(":"))
+                    dim = dim.substring(dim.indexOf(":")+1);
+
+                tooltip.add(new TextComponent(String.format("(x=%d, y=%d, z=%d) in %s", coordX, coordY, coordZ, dim)));
+            }
+        } else {
+            tooltip.add(new TextComponent("Forget me not.."));
+        }
     }
 }
