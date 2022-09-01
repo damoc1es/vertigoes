f9d3326070295a6ec89ebc3c1267419159788c11
Damocles <38077049+damoc1es@users.noreply.github.com>
2022-09-01 20:22:36 +0300

    added the primal, plant and death pearls

---
+++ src/main/java/dev/damocles/vertigoes/item/DeathPearl.java
@@ -0,0 +1,23 @@
+package dev.damocles.vertigoes.item;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class DeathPearl extends Item {
+    public DeathPearl(Properties pProperties) {
+        super(pProperties);
+    }
+
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltipComponents, @NotNull TooltipFlag isAdvanced) {
+        tooltipComponents.add(new TextComponent("Anti-life").withStyle(ChatFormatting.GRAY));
+    }
+}
---
+++ src/main/java/dev/damocles/vertigoes/item/PlantPearl.java
@@ -0,0 +1,24 @@
+package dev.damocles.vertigoes.item;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class PlantPearl extends Item {
+
+    public PlantPearl(Properties pProperties) {
+        super(pProperties);
+    }
+
+    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltipComponents, @NotNull TooltipFlag isAdvanced) {
+        tooltipComponents.add(new TextComponent("Plant Life").withStyle(ChatFormatting.GRAY));
+    }
+}
