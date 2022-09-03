f5f4c60c008ec10091ae4d4f8d84ada9f354e4d9
Damocles <38077049+damoc1es@users.noreply.github.com>
2022-09-03 12:22:50 +0300

    on use of plant pearl now places plant essence block, infinite use but can't be picked up and breaks instantly if destroyed using the pearl

--- src/main/java/dev/damocles/vertigoes/item/PlantPearl.java
+++ src/main/java/dev/damocles/vertigoes/item/PlantPearl.java
@@ -3,19 +3,41 @@ package dev.damocles.vertigoes.item;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
+import static dev.damocles.vertigoes.setup.Registration.*;
+
 public class PlantPearl extends Item {
 
-    public PlantPearl(Properties pProperties) {
-        super(pProperties);
+    public PlantPearl(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public @NotNull InteractionResult useOn(@NotNull UseOnContext context) {
+        BlockHitResult hitResult = new BlockHitResult(context.getClickLocation(), context.getClickedFace(), context.getClickedPos(), context.isInside());
+        BlockPlaceContext newContext = new BlockPlaceContext(context.getLevel(), context.getPlayer(), context.getHand(), PLANT_ESSENCE_ITEM.get().getDefaultInstance(), hitResult);
+
+        return PLANT_ESSENCE_ITEM.get().useOn(newContext);
+    }
+
+    @Override
+    public float getDestroySpeed(@NotNull ItemStack stack, BlockState state) {
+        if(state.is(PLANT_ESSENCE.get()))
+            return 16.0F;
+        return super.getDestroySpeed(stack, state);
     }
 
     public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltipComponents, @NotNull TooltipFlag isAdvanced) {
